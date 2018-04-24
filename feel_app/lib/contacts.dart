8a3888d8288621eb2f2b67f342a944fc1b2e2764
Fenix <firebirdatheart@gmail.com>
2018-04-23 20:46:56 -0600

    tyessssss

--- feel_app/lib/contacts.dart
+++ feel_app/lib/contacts.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:feel_app/feel_navigation_bar.dart';
+import 'package:feel_app/theme.dart';
 
 class ContactsWidget extends StatelessWidget {
 
@@ -14,14 +15,49 @@ class ContactsWidget extends StatelessWidget {
           child: new Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget> [
-                new Text("Mental Health Helpline"),
+                new Text(
+                    'Resources',
+                style: new TextStyle(
+                  fontFamily: 'Dosis',
+                  fontSize: 32.0,
+                  fontWeight: FontWeight.bold,
+                  color: FeelColors.peach[700])
+                ),
+                new Text (" "),
+                new Text(" "),
+                new Text(
+                  'Alberta',
+                  style: new TextStyle(
+                    fontFamily: 'Dosis',
+                    fontSize: 15.0,
+                    fontWeight: FontWeight.bold,
+                    color: FeelColors.peach[700]
+        ),
+                  ),
+
+                new Text(
+                    'Mental Health Helpine',
+                    style: new TextStyle(
+                        fontFamily: 'Dosis',
+                        fontSize:20.0,
+                        fontWeight: FontWeight.bold,
+                        color: FeelColors.peach[700])),
                 new Text("1-877-303-2642 (24/7)"),
                 new Text("  "),
-                new Text("Crisis/Distress Lines"),
+                new Text(
+                    'Crisis/Distress Lines',
+                        style: new TextStyle(
+                    fontFamily: 'Dosis',
+                  fontSize: 20.0,
+                          fontWeight: FontWeight.bold,
+                          color: FeelColors.peach[700]
+                ),
+                ),
                 new Text("Edmonton: 780-482-4357; 780-342-7777"),
                 new Text("Calgary: 403-266-HELP (4357)"),
                 new Text("  "),
-                new Text
+                new Text("ConnectTeen: text, chat, phone, email"),
+                new Text("Distress Centre Calgary: chat, phone, email")
               ]
           )
         ),
