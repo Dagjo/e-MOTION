a75f4fd9010f10857d3b875ec92299ad5763ed8e
Fenix <firebirdatheart@gmail.com>
2018-04-24 19:08:16 -0600

    Resources Page

--- feel_app/lib/contacts.dart
+++ feel_app/lib/contacts.dart
@@ -24,15 +24,13 @@ class ContactsWidget extends StatelessWidget {
                   fontWeight: FontWeight.bold,
                   color: FeelColors.peach[700])
                 ),
-                new Text (" "),
                 new Text(" "),
                 new Text(
-                  'Alberta',
+                  'ALBERTA',
                   style: new TextStyle(
                     fontFamily: 'Dosis',
                     fontSize: 15.0,
                     fontWeight: FontWeight.bold,
-                    color: FeelColors.peach[700]
         ),
                   ),
 
@@ -45,7 +43,6 @@ class ContactsWidget extends StatelessWidget {
                         color: FeelColors.peach[700])),
                 new FlatButton(onPressed: () => {}, child:
                 new Text("1-877-303-2642 (24/7)")),
-                new Text("  "),
                 new Text(
                     'Crisis/Distress Lines',
                         style: new TextStyle(
@@ -55,11 +52,31 @@ class ContactsWidget extends StatelessWidget {
                           color: FeelColors.peach[700]
                 ),
                 ),
-                new Text("Edmonton: 780-482-4357; 780-342-7777"),
-                new Text("Calgary: 403-266-HELP (4357)"),
+                new Text("Edmonton"),
+                new Text("Distress Line: (24h) 780-482-HELP (4357)"),
+                new Text("Crisis Response Team: 780-342-7777"),
                 new Text("  "),
-                new Text("ConnectTeen: text, chat, phone, email"),
-                new Text("Distress Centre Calgary: chat, phone, email")
+                new Text("Calgary"),
+                new Text("ConnectTeen: (24h) 403-264-TEEN (8336)"),
+                new Text("Distress Centre Calgary: 403-266-HELP (4357)"),
+                new Text("  "),
+                new Text(
+                  'BC, ALBERTA and SASK also use',
+                  style: new TextStyle(
+                    fontFamily: 'Dosis',
+                    fontSize: 15.0,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                new Text(
+                  '811',
+                  style: new TextStyle(
+                    fontFamily: 'Dosis',
+                    fontSize: 48.0,
+                    fontWeight: FontWeight.bold,
+                      color: FeelColors.peach[700]
+                  ),
+                ),
               ]
           )
         ),
