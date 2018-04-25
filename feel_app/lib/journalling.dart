8cef20b87fecc6ed50b0904cc35d555baa162cb3
Damien Jones <damien.jones@3esi-Enersight.com>
2018-04-24 18:21:24 -0600

    Pop an alert when a button is pressed

--- feel_app/lib/journalling.dart
+++ feel_app/lib/journalling.dart
@@ -12,21 +12,21 @@ class JournallingWidget extends StatelessWidget {
         body: new ListView.builder(
           itemBuilder: (BuildContext context, int index) {
             if (index == 0)
-              return createCard (asset: "graphics/bear1.png", title: "09-16-17", subtitle: "Caleb and I shared the secret of our crushes today. He likes... You thought I'd tell? ;)");
+              return createCard (context: context, asset: "graphics/bear1.png", title: "09-16-17", subtitle: "Caleb and I shared the secret of our crushes today. He likes... You thought I'd tell? ;)");
             if (index == 1)
-              return createCard (asset: "graphics/bear1.png", title: "09-14-17", subtitle: "Today was great! My new friend Caleb came I were hanging out when he est of my friends came over and eveyone go along!");
+              return createCard (context: context, asset: "graphics/bear1.png", title: "09-14-17", subtitle: "Today was great! My new friend Caleb came I were hanging out when he est of my friends came over and eveyone go along!");
             if (index == 2)
-              return createCard (asset: "graphics/bear2.png", title: "09-12-17", subtitle: "Today was better, I sat with a new kid and now we're friends.");
+              return createCard (context: context, asset: "graphics/bear2.png", title: "09-12-17", subtitle: "Today was better, I sat with a new kid and now we're friends.");
             if (index == 3)
-              return createCard (asset: "graphics/bear3.png", title: "09-10-17", subtitle: "Today was alright, no one punched me and I'm allowed back in school.");
+              return createCard (context: context, asset: "graphics/bear3.png", title: "09-10-17", subtitle: "Today was alright, no one punched me and I'm allowed back in school.");
             if (index == 4)
-              return createCard (asset: "graphics/bear3.png", title: "09-08-17", subtitle: "Today was a bad day. A bully punched me in the nose. I punched him back. Got suspended.");
+              return createCard (context: context, asset: "graphics/bear3.png", title: "09-08-17", subtitle: "Today was a bad day. A bully punched me in the nose. I punched him back. Got suspended.");
             if (index == 5)
-              return createCard (asset: "graphics/bear4.png", title: "09-06-17", subtitle: "Today was okay I guess. I forgot my binder at home, and I think my friends were ignoring me.");
+              return createCard (context: context, asset: "graphics/bear4.png", title: "09-06-17", subtitle: "Today was okay I guess. I forgot my binder at home, and I think my friends were ignoring me.");
                 }),
               bottomNavigationBar: new FeelNavigationBar(currentIndex: 1));
   }
-  Card createCard({asset: String, title: String, subtitle: String}){
+  Card createCard({context: BuildContext, asset: String, title: String, subtitle: String}){
     return new Card(
         child: new Column(
             mainAxisSize: MainAxisSize.min,
@@ -44,16 +44,23 @@ class JournallingWidget extends StatelessWidget {
                   child: new ButtonBar(
                       children: <Widget>[
                         new IconButton(
-                            icon: new Icon(Icons.create),
-                          onPressed: () {/*...*/}
+                          icon: new Icon(Icons.create),
+                          onPressed: () => _showAlert(context, title, "Edit")
                         ),
                         new IconButton(
                           icon: new Icon(Icons.delete),
-                          onPressed: () {/*...*/}
+                          onPressed: () => _showAlert(context, title, "Delete")
                         ),
                       ]
                   ))
             ])
     );
+  }
+
+  _showAlert(BuildContext context, String title, String text) {
+    showDialog(context: context, child: new AlertDialog(
+        title: new Text(title),
+        content: new Text(text),
+      ));
   }
 }
